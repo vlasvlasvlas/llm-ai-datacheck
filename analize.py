098d482b74739962ad0cd0e45dbd7bbb27cf61fd
Vladimiro Bellini <vbellini@mail.austral.edu.ar>
2024-01-12 11:05:53 -0300

    more testing

--- analize.py
+++ analize.py
@@ -96,39 +96,60 @@ data_checks = [
 ]
 print("data_checks:", data_checks)
 
+# General prompt
+general_prompt = """
+You are leading the development of a global AI accountability index, focusing on analyzing legal documents for insights into AI and ethics. Maintain a serious and precise approach, acknowledging uncertainties. Stay neutral in perspective, providing comprehensive context when possible. Ensure your responses reflect an IQ of at least 150. Use the provided Markdown format for thematic guidance and tools to identify key elements in law documents.
+"""
+    
 # AI definition for analysis
-ai_definition = "For purposes of this study, evidence relating to AI will be understood in a broad sense. For example, when researchers are requested to analyze frameworks related to AI, accepted evidence will be those frameworks that use language referring to ‘Artificial Intelligence’ to define their object, or those that cover activities fitting in the above-mentioned definition, regardless of whether the specific term ‘Artificial Intelligence’ has been used. For example, if there is mention of ‘machine learning’ or ‘algorithmic modeling’ but no mention of ‘artificial intelligence’ in a document, please consider these as evidence relating to AI since they mention AI-driven processes"
+ai_definition = "For the purpose of this study, the scope of evidence pertaining to AI is expansively defined. When researchers are tasked with examining frameworks associated with AI, accepted evidence encompasses frameworks using the term 'Artificial Intelligence' for their definition. Additionally, evidence includes frameworks covering activities aligning with the aforementioned definition, irrespective of explicit use of the term 'Artificial Intelligence.' For instance, references to 'machine learning' or 'algorithmic modeling' in a document without explicit mention of 'artificial intelligence' should still be considered as evidence related to AI, given their association with AI-driven processes."
 
 # Question prompt for AI
-question = """
-Kindly provide clear and concise responses to the following queries.
+"""
+qv1:
 Output Format: CSV Pipe separated UTF-8 with the following columns HEADER: answer_1_fullanswer | answer_2_yesno | answer_2_fullanswer | answer_3_yesno | answer_3_fullanswer | answer_4_yesno | answer_4_fullanswer | answer_5_doc_date 
 - ALWAYS include the column names in the first row of the CSV file.
 - ALWAYS USE pipe (|) as the separator AND double quote delimiter (") for data.
 - Yes/No answers (columns that ends with _yesno) must be answered with a Yes or No.
 - Full answers (columns that ends with _fullanswer) must not be cutted or truncated ensuring that these columnbased answers are complete and full extense.
 - Must return 1 row per document analyzed.
+- ALWAYS remove linebreaks from inside each column.
+- ALWAYS check expected fields. they MUST be 8. If they are more, PLEASE CHECK POSSIBLE LINE BREAKS INSIDE THE TEXT OF EACH COLUMN AND REMOVE THEM.
 
+Kindly provide clear and concise responses to the following queries.
 Please respond in fluent English and initiate your responses with a definitive answer for each specific question:
 
 1) Identify the thematic area outlined in the markdown guide for analysis.
-
 2) Determine with utmost clarity whether the document precisely addresses the topic of artificial intelligence in a specific and unambiguous manner, referencing the provided definition: {ai_definition}.
-
 3) Conclusively establish if the document specifically addresses the identified thematic area.  Always provide a detailed explanation of how the document specifically addresses the indicated thematic area outlined in the markdown rule guide.
-
 4) Provide a detailed explanation of how the document addresses the specified thematic area outlined in the markdown rule guide. Ensure your response includes specific references to the relevant chapter and article of the document.
-
 5) Find the release date or last update date of the document.
 
 Responses should be expressed in a extense professional, precise, and neutral manner, with a particular emphasis on clarity to ensure the highest reliability and quality in your answers.
+"""
 
+# qv2:
+question = """
+Kindly provide clear and concise responses to the following queries.
+Please respond in fluent English and initiate your responses with a definitive answer for each specific question:
+
+1) Please identify clearly the thematic area that you would like to analyze.
+2) Does the document address the topic of "artificial intelligence" in a specific and unambiguous manner? please you must use the term as the following definition: **{ai_definition}**.
+3) Please provide a detailed explanation of how the document specifically addresses the identified thematic area outlined in the markdown rule guide? Please relate your response using the identified thematic area outlined in the markdown rule guide and the provided definition of artificial intelligence.
+4) Please provide a detailed explanation of how the document addresses the specified thematic area outlined in the markdown rule guide and the provided definition of artificial intelligence. Ensure your response includes specific references to the relevant chapter and article of the document.
+5) What is the earliest release date of the document?
+
+Responses should be expressed in a professional, precise, and neutral manner, with a particular emphasis on clarity to ensure the highest reliability and quality in your answers.
 """.format(
     ai_definition=ai_definition
 )
 
+
+
 # prompt to fix and repair a csv data file text
-refining_output = """
+
+"""
+#refining v1:
 You are the best data analyzer and CSV fixer.
 gold rules:
 - You must fix the content that is given to you, its suposed to be a CSV with 1 header and 1 row.
@@ -138,16 +159,45 @@ gold rules:
 - ALWAYS remove linebreaks from inside each column.
 - ALWAYS include the column names in the first row of the CSV file.
 - ALWAYS USE pipe (|) as the separator AND double quote delimiter (") for data.
-- NEVER change the meaning of the text given to you, you must only fix the CSV format!.
+- NEVER change the meaning of the text given to you, you must only arrange and fix the format of the input to CSV format!.
 - ALWAYS check expected fields. they MUST be 8. If they are more, PLEASE CHECK POSSIBLE LINE BREAKS INSIDE THE TEXT OF EACH COLUMN AND REMOVE THEM.
 As output I only need the header and the row, you must not add anything to the response. Give your best!
+"""
+
+# refining v2:
+refining_output = """
+You are the best data analyzer and CSV fixer.
+Gold rules:
+- You must fix the content that is given to you. It's supposed to be a CSV with 1 header and 1 row.
+- Output format needed: CSV Pipe separated UTF-8 with the following columns HEADER: answer_1_fullanswer | answer_2_yesno | answer_2_fullanswer | answer_3_yesno | answer_3_fullanswer | answer_4_yesno | answer_4_fullanswer | answer_5_doc_date 
+- For answer_1_fullanswer, please provide only the name of the thematic area and nothing else.
+- for _yesno columns please extract the answer from the specific question and do not change the answer or the meaning of the answer.
+- For answer_5_doc-date, you must find and fix the date: it must be in the following format: YYYY-MM-DD
+- If columns exist and are correct, you must not change them.
+- Always remove linebreaks from inside each column.
+- Always include the column names in the first row of the CSV file.
+- Always use pipe (|) as the separator AND double quote delimiter (") for data.
+- Never change the meaning of the text given to you, you must only fix the CSV format!.
+- Always check expected fields. They must be 8. If they are more, please check possible line breaks inside the text of each column and remove them.
+As output, I only need the header and the row. You must not add anything to the response. Give your best!
 """.format()
 
-refining_answer = "please fix the input and prepare it as the CSV i need based on the rules i gave you"
+#refining v1 
+#refining_answer = "please fix the input and prepare it as the CSV i need based on the rules i gave you"
+
+#refining v2
+refining_answer = "Please fix the input and prepare it as a CSV file based on the rules I gave you. As output, I only need the header and the row. You must not add anything to the response. Give your best! I need  100% working CSV output with 0 errors."
 
 
 # Function to ask questions using OpenAI's ChatCompletion
 def ask_question(bible_prompt, text, question):
+    """
+    Ask a question using OpenAI's ChatCompletion.
+    Inputs: 
+        bible_prompt: The system prompt to use for the AI.
+        text: The text input to use for the AI.
+        question: The question to ask the AI.
+    """
     print("asking question..")
 
     response = openai.ChatCompletion.create(
@@ -183,6 +233,8 @@ df_master = pd.DataFrame(
 
 # Iterate over data_file in the 'data' folder
 for data_file in data_files:
+
+    # objective: read text from data_file and ask question to AI
     print("analyzing file:", data_file)
 
     # Read text from data_file
@@ -205,9 +257,10 @@ for data_file in data_files:
 
         # Bible prompt
         bible_prompt = """
-        You are working on the development of a global index of accountability in artificial intelligence. Specifically, you are analyzing specific thematic areas. Your approach must be highly serious and precise. If there is something you do not know or are not entirely clear about, you must explicitly state that you do not know. Your perspective on the topic should remain neutral. Whenever possible, provide a comprehensive context for your response. You are required to respond with an IQ of at least 150. The following Markdown format will guide you in understanding what the thematic area entails and provide tools to identify elements of the thematic area in any document.
+        {general_prompt}
         -- BEGIN MARKDOWN RULES GUIDE -- {textcheck} -- END MARKDOWN RULES GUIDE --
         """.format(
+            general_prompt=general_prompt,
             textcheck=textcheck
         )
 
